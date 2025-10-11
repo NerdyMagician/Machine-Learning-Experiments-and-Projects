36ef797ae1ec5a112f74406ce9bcd2ea86b7c970
Rudransh-Sharma-30 <rudra.s3009@gmail.com>
2025-10-12 01:27:57 +0530

    update

---
+++ DL.py
@@ -0,0 +1,44 @@
+import numpy as np
+import pandas as pd
+from sklearn.datasets import fetch_openml
+
+from sklearn.datasets import fetch_openml
+X, y = fetch_openml('mnist_784', version=1,
+return_X_y=True)
+X = X.values
+
+# print(X.shape)      # should be (70000, 784)
+# print(X.head())     # should show real pixel numbers, not zero
+# non_zero_rows = np.where(X.values.sum(axis=1) != 0)[0]
+
+# if len(non_zero_rows) == 0:
+#     print("All rows contain only zeros — dataset likely not loaded correctly.")
+# else:
+#     first_index = non_zero_rows[0]
+#     print(f"First non-zero row index: {first_index}")
+#     print(X.iloc[first_index])
+y = y.astype(int).values
+X = ((X/255.)-5)*2
+
+import matplotlib.pyplot as plt
+fig , ax = plt.subplots(nrows = 2, ncols =5 , sharex=True , sharey= False)
+ax = ax.flatten()
+for i in range(10):
+    img = X[y == i][0].reshape(28,28)
+    ax[i].imshow(img , cmap = 'Greys')
+ax[0].set_xticks([])
+ax[0].set_yticks([])
+plt.tight_layout()
+# plt.show()
+fig, ax = plt.subplots(nrows=5,ncols=5,sharex=True,sharey=True)
+ax = ax.flatten()
+for i in range(25):
+    img = X[y == 7][i].reshape(28, 28)
+    ax[i].imshow(img, cmap='Greys')
+    ax[0].set_xticks([])
+    ax[0].set_yticks([])
+plt.tight_layout()
+plt.show()
+from sklearn.model_selection import train_test_split
+X_temp, X_test, y_temp, y_test = train_test_split(X,y,test_size=10000,random_state=123, stratify=y)
+from neuralnet import NeuralNetMLP
